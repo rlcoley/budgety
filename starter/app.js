b5fbbf6435428cece5c06f4a8f3e0ba9cdeb937d
rlcoley <rosscoley1002@gmail.com>
2019-01-28 00:21:07 -0500

    toggling color of fields based on if input is income or expense

--- starter/app.js
+++ starter/app.js
@@ -200,6 +200,12 @@ var UIController = (function() {
     return (type === 'exp' ? '-' : '+') + ' ' + int + '.' + dec
   };
 
+  var nodeListForEach = function(list, callback) {
+    for (var i = 0; i < list.length; i++) {
+      callback(list[i], i)
+    }
+  };
+
   return{
       getInput: function(){
           return{
@@ -290,14 +296,6 @@ var UIController = (function() {
 
         var fields = document.querySelectorAll(DOMstrings.expensesPercLabel);
 
-
-        var nodeListForEach = function(list, callback) {
-          for (var i = 0; i < list.length; i++) {
-            callback(list[i], i)
-          }
-        }
-
-
         nodeListForEach(fields, function(current, index) {
 
           if (percentages[index] > 0) {
@@ -319,6 +317,21 @@ var UIController = (function() {
 
             year = now.getFullYear();
             document.querySelector(DOMstrings.dateLabel).textContent = months[month] + ' ' + year;
+        },
+
+        changedType: function() {
+
+            var fields = document.querySelectorAll(
+              DOMstrings.inputType + ',' +
+              DOMstrings.inputDescription + ',' +
+              DOMstrings.inputValue
+            );
+
+            nodeListForEach(fields, function(cur) {
+                cur.classList.toggle('red-focus');
+            });
+
+            document.querySelector(DOMstrings.inputBtn).classList.toggle('red');
         },
 
       getDOMstrings: function() {
@@ -344,6 +357,8 @@ var controller = (function (budgetCtrl, UICtrl) {
     });
 
     document.querySelector(DOM.container).addEventListener('click', ctrlDeleteItem);
+
+    document.querySelector(DOM.inputType).addEventListener('change', UICtrl.changedType);
   };
 
 
